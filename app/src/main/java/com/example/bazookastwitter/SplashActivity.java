1681d700d6c3ab0bb300d772adc0298ca45e620a
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2019-02-20 12:09:26 +0100

    Splash working

--- app/src/main/java/com/example/bazookastwitter/SplashActivity.java
+++ app/src/main/java/com/example/bazookastwitter/SplashActivity.java
@@ -1,6 +1,22 @@
 package com.example.bazookastwitter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
+
+    private final String LOG_TAG = "myapp:splashActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.v(LOG_TAG, "Splash here!");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
