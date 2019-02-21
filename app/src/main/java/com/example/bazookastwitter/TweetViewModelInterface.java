bc9dad026b76f0e132541be63bd9362614062e1f
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2019-02-21 11:38:52 +0100

    Created TweetViewModelInterface
    might come in handy if something else needs to be displayed

---
+++ app/src/main/java/com/example/bazookastwitter/TweetViewModelInterface.java
@@ -0,0 +1,17 @@
+package com.example.bazookastwitter;
+
+import android.support.annotation.NonNull;
+
+import twitter4j.Status;
+
+public interface TweetViewModelInterface {
+    @NonNull
+    public String getHeaderText();
+    public void setHeaderText(@NonNull final String headerText);
+    @NonNull
+    public String getBodyText();
+    public void setBodyText(@NonNull final String bodyText);
+    @NonNull
+    public String getDateText();
+    public void setDateText(@NonNull final String dateText);
+}
