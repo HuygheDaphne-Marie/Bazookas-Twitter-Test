8723889c96de008f9b65bdaf99e3ed5cdf67ee51
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2019-02-21 11:47:58 +0100

    Removed unused TweetDisplayActivity interface

--- app/src/main/java/com/example/bazookastwitter/TweetDisplayActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.bazookastwitter;
-
-import java.util.List;
-
-import twitter4j.Status;
-
-public interface TweetDisplayActivity {
-    void AddTweetsToView(final List<Status> statuses);
-}
