f8c40ef557c6afdfda62c48f59d4795ff6b3fe6b
HuygheHenri-Thibault <henrithibault.huyghe@gmail.com>
2019-02-21 16:54:21 +0100

    TweetViewModel & Holder can now handle images

--- app/src/main/java/com/example/bazookastwitter/displayTweet/TweetViewHolder.java
+++ app/src/main/java/com/example/bazookastwitter/displayTweet/TweetViewHolder.java
@@ -3,24 +3,38 @@ package com.example.bazookastwitter.displayTweet;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bazookastwitter.R;
+import com.koushikdutta.ion.Ion;
 
 public class TweetViewHolder extends RecyclerView.ViewHolder {
     private TextView headerTextView;
+    private ImageView imageView;
     private TextView bodyTextView;
     private TextView dateTextView;
 
     public TweetViewHolder(@NonNull View itemView) {
         super(itemView);
         headerTextView = itemView.findViewById(R.id.header_text);
+        imageView = itemView.findViewById(R.id.content_image);
         bodyTextView = itemView.findViewById(R.id.body_text);
         dateTextView = itemView.findViewById(R.id.date_text);
     }
 
     public void bindData(final TweetViewModelInterface viewModel) {
         headerTextView.setText(viewModel.getHeaderText());
+        if(imageView != null && viewModel.getImg() != null) { // TODO second clause might be redundant..
+            imageView.setImageBitmap(viewModel.getImg());
+            // TODO Ion images kept disappearing randomly, probably because Ion couldn't get them quick enough
+//            Ion.with(imageView)
+////                    .placeholder(R.drawable.placeholder_image)
+////                    .error(R.drawable.error_image) // TODO https://github.com/koush/ion#load-an-image-into-an-imageview
+////                    .animateLoad(spinAnimation)
+////                    .animateIn(fadeInAnimation)
+//                    .load(viewModel.getImgUrl());
+        }
         bodyTextView.setText(viewModel.getBodyText());
         dateTextView.setText(viewModel.getDateText());
     }
--- app/src/main/java/com/example/bazookastwitter/displayTweet/TweetViewModel.java
+++ app/src/main/java/com/example/bazookastwitter/displayTweet/TweetViewModel.java
@@ -1,21 +1,33 @@
 package com.example.bazookastwitter.displayTweet;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import twitter4j.MediaEntity;
 import twitter4j.Status;
+import twitter4j.URLEntity;
 
 public class TweetViewModel implements TweetViewModelInterface {
+    private final String LOG_TAG = "myapp:TweetViewModel";
     private String headerText;
+    private Bitmap img;
     private String bodyText;
     private String dateText;
 
     public TweetViewModel(@NonNull final Status tweet) {
         setHeaderText("@"+tweet.getUser().getScreenName());
-        setBodyText(tweet.getText());
+        handleMedia(tweet);
+        setBodyText(cleanupBodyText(tweet));
         DateFormat df = new SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH);
         setDateText(df.format(tweet.getCreatedAt()));
     }
@@ -40,5 +52,39 @@ public class TweetViewModel implements TweetViewModelInterface {
     }
     public void setDateText(@NonNull final String dateText) {
         this.dateText = dateText;
+    }
+
+    public Bitmap getImg() {
+        return img;
+    }
+    public void setImg(@NonNull final String imgUrl) {
+        new Thread(new Runnable() { // FIXME this is dirtier than your mama *yikes*
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(imgUrl);
+                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    img = bmp;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private String cleanupBodyText(@NonNull Status tweet) {
+        String tweetText = tweet.getText();
+        for(URLEntity url : tweet.getURLEntities()) {
+            tweetText = tweetText.replace(url.getURL(), url.getDisplayURL());
+        }
+        return tweetText.split(" https://t.co")[0];
+    }
+    private void handleMedia(@NonNull Status tweet) {
+        MediaEntity[] media = tweet.getMediaEntities();
+        if (media.length > 0) {
+            setImg(media[0].getMediaURL());
+        }
     }
 }
